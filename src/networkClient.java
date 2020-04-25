5def94857ebfd23089d7957f21f71246c027c5b1
dasMichal <57588949+dasmichal@users.noreply.github.com>
2020-04-25 06:53:08 +0200

    Socket funktioniert.
    ヽ(•‿•)ノ

--- src/networkClient.java
+++ src/networkClient.java
@@ -8,33 +8,32 @@ public class networkClient
 
     public static void main(String[] args) throws IOException {
 
-        Scanner input = new Scanner(System.in);
-        String eingabe;
-        int num=0;
-        try{
-            Socket socket = new Socket("localhost",6666);
-            DataOutputStream dout =new DataOutputStream(socket.getOutputStream());
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    Scanner input = new Scanner(System.in);
+    String eingabe;
+    int num=0;
+    try{
+        Socket socket = new Socket("localhost",6666);
+        DataOutputStream dout =new DataOutputStream(socket.getOutputStream());
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            do
-            {
-                System.out.print("Gebe einen Text ein: ");
-                eingabe = input.nextLine();
-                out.println(eingabe);
-                //dout.writeUTF(eingabe);
-                num++;
-            }while(!eingabe.equalsIgnoreCase("exit"));
+        do
+        {
+            System.out.print("Gebe einen Text ein: ");
+            eingabe = input.nextLine();
+            out.println(eingabe);
+            //dout.writeUTF(eingabe);
+            num++;
+        }while(!eingabe.equalsIgnoreCase("exit"));
 
 
+        //dout.writeUTF("Hello Server");
+        //dout.flush();
+        //dout.close();
+        socket.close();
+    }catch(Exception e){System.out.println(e);}
 
-            //dout.writeUTF("Hello Server");
-            dout.flush();
-            dout.close();
-            socket.close();
-        }catch(Exception e){System.out.println(e);}
-
-    }
+}
 
 
     public static void sendMessage(int player, int column)
@@ -58,13 +57,12 @@ public class networkClient
 
                 ausgabe = (playerS+","+columnS);
                 out.printf(ausgabe);
-                out.flush();
-                System.out.println(ausgabe);
+                //out.flush();
+                //System.out.println(ausgabe);
                 //out.println("CLR");
                 //dout.writeUTF(eingabe);
 
            // }while(!ausgabe.equalsIgnoreCase("exit"));
-
 
 
             //dout.writeUTF("Hello Server");
---
+++ src/networkserver2.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+
+public class networkserver2
+{
+	//static ServerSocket variable
+	private static ServerSocket server;
+	//socket server port on which it will listen
+	private static int port = 6666;
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException{
+		//create the socket server object
+		server = new ServerSocket(port);
+		//keep listens indefinitely until receives 'exit' call or program terminates
+		while(true){
+			System.out.println("Waiting for the client request");
+			//creating socket and waiting for client connection
+			Socket socket = server.accept();
+
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String message = in.readLine();
+			String[] data;
+
+
+			//System.out.println(">>> " + message);
+			try
+			{
+				data = message.split(",");
+				System.out.println(">>> " + message);
+				System.out.println(">>> Player " + data[0] + " Column " + data[1]);
+			} catch (Exception e)
+			{
+				//System.out.println("oups no splitting");
+			}
+			//out.close();
+			//in.close();
+			socket.close();
+			//terminate the server if client sends exit request
+			if(message.equalsIgnoreCase("exit")) break;
+		}
+		System.out.println("Shutting down Socket server!!");
+		//close the ServerSocket object
+		server.close();
+	}
+}
