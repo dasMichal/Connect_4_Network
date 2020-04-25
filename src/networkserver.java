b7d7593b3cc7ec2e29623a5ed45b442d6f394106
dasMichal <michalxd@live.de>
2020-04-25 08:38:42 +0200

    Kleiner Fix am Server1
    ヽ(•‿•)ノ

--- src/networkserver.java
+++ src/networkserver.java
@@ -3,46 +3,50 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class networkserver {
+    //static ServerSocket variable
+    private static ServerSocket server;
+    //socket server port on which it will listen
+    private static int port = 6666;
 
-    public static void main(String[] args) throws IOException {
-
-        try{
-            String message = "a";
-            while(true)
-            {
-                ServerSocket serverSocket = new ServerSocket(666);
-                Socket socket = serverSocket.accept();//establishes connection
-                //PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    public static void main(String[] args) throws IOException
+    {
 
 
-                String[] data;
-                while((message = in.readLine()) != null)
-                {
-                    try
-                    {
-                        data = message.split(",");
-                        System.out.println(">>> " + message);
-                        System.out.println(">>> Player " + data[0] + " Column " + data[1]);
-                    } catch (Exception e)
-                    {
-                        //System.out.println("oups no splitting");
-                    }
-
-                }
-
-                //DataInputStream dis=new DataInputStream(socket.getInputStream());
-            }
-
-
-           // serverSocket.close();
-        }catch(Exception e)
+        //create the socket server object
+        server = new ServerSocket(port);
+        //keep listens indefinitely until receives 'exit' call or program terminates,
+        Socket socket = server.accept();
+        while (true)
         {
-            //System.out.println(e);
-            e.printStackTrace();
-        }
+            //System.out.println("Waiting for the client request");
+            //creating socket and waiting for client connection
+            //Socket socket = server.accept();
 
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String message = in.readLine();
+            String[] data;
+
+
+            //System.out.println(">>> " + message);
+            try
+            {
+                data = message.split(",");
+                System.out.println(">>> " + message);
+                System.out.println(">>> Player " + data[0] + " Column " + data[1]);
+            } catch (Exception e)
+            {
+                //System.out.println("oups no splitting");
+            }
+            //out.close();
+            //in.close();
+            //socket.close();
+            //terminate the server if client sends exit request
+            if (message.equalsIgnoreCase("exit")) break;
+        }
+        System.out.println("Shutting down Socket server!!");
+        //close the ServerSocket object
+        server.close();
 
     }
-
     }
